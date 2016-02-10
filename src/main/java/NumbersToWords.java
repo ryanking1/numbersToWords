a70716270c239af2b3e0219ea5cf9029b7c503d8
Ryan King and trevor elvey <ryanking157@hotmail.com>
2016-02-09 16:43:20 -0800

    add passing specs for zero to ninety except for teens

--- src/main/java/NumbersToWords.java
+++ src/main/java/NumbersToWords.java
@@ -23,18 +23,49 @@ public static String inputNumber(String number) {
   ones.put("8", "eight");
   ones.put("9", "nine");
 
+  HashMap<String, String> teens = new HashMap<String, String>();
+
+  teens.put("0", "ten");
+  teens.put("1", "eleven");
+  teens.put("2", "twelve");
+  teens.put("3", "thirteen");
+  teens.put("4", "fourteen");
+  teens.put("5", "fifteen");
+  teens.put("6", "sixteen");
+  teens.put("7", "seventeen");
+  teens.put("8", "eighteen");
+  teens.put("9", "nineteen");
+
   String onesPlace = "";
+  String onesResult = "";
+  String tensResult = "";
+
+  HashMap<String, String> tens = new HashMap<String, String>();
+
+
+  tens.put("2", "twenty");
+  tens.put("3", "thirty");
+  tens.put("4", "forty");
+  tens.put("5", "fifty");
+  tens.put("6", "sixty");
+  tens.put("7", "seventy");
+  tens.put("8", "eighty");
+  tens.put("9", "ninety");
+
+  String tensPlace = "";
 
   char[] charArray = number.toCharArray();
   for ( int i = charArray.length -1; i >= 0; i--){
     String numberValue = (Character.toString(charArray[i]));
-      if (ones.containsKey(numberValue)) {
+      if ((i == 0) && (ones.containsKey(numberValue))) {
         String value = ones.get(numberValue);
-        onesPlace = onesPlace.concat(value);
-      } else {
-        return onesPlace;
+        onesResult = onesPlace.concat(value);
+      }if ((i == 1) && (tens.containsKey(numberValue))) {
+        String tensValue = tens.get(numberValue);
+        tensResult = tensValue.concat(onesPlace);
       }
     }
-    return onesPlace;
+    String result = tensResult.concat(onesResult);
+    return result;
   }
 }
--- src/test/java/NumbersToWordsTest.java
+++ src/test/java/NumbersToWordsTest.java
@@ -5,8 +5,13 @@ import java.util.*;
 public class NumbersToWordsTest {
 
   @Test
-  public void numbersToWords_returnAsWordforNumber_1() {
+  public void numbersToWords_returnAsWordforNumber_3() {
     NumbersToWords numbersToWords= new NumbersToWords();
-    assertEquals("one", numbersToWords.inputNumber("1"));
+    assertEquals("three", numbersToWords.inputNumber("3"));
+  }
+  @Test
+  public void numbersToWords_returnAsWordforNumber_22() {
+    NumbersToWords numbersToWords= new NumbersToWords();
+    assertEquals("twentytwo", numbersToWords.inputNumber("22"));
   }
 }
